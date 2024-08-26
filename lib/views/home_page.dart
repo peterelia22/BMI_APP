c38a4ec1acffdefae2600a4345f42765ae5b01f5
peterelia22 <peterelia914@gmail.com>
2024-08-26 15:28:04 +0300

    some icons in home

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,8 +5,10 @@ import 'package:bmi/views/result_page.dart';
 import 'package:bmi/widgets/age_and_weight.dart';
 import 'package:bmi/widgets/height.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../widgets/genderbox.dart';
 
@@ -25,6 +27,7 @@ class _HomePageState extends State<HomePage> {
 
   bool _isFinished = false;
   double _bmiScore = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,81 +39,121 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: SingleChildScrollView(
+      body: SizedBox.expand(
         child: Container(
           padding: const EdgeInsets.all(12),
-          child: Card(
-            elevation: 10,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-            child: Column(
-              children: [
-                Gender(
-                  onChanged: (genderVal) {
-                    _gender = genderVal;
-                  },
-                ),
-                Height(
-                  onChanged: (heightVal) {
-                    _height = heightVal;
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AgeAndWeight(
-                      title: "Age",
-                      onChange: (ageVal) {
-                        _age = ageVal;
-                      },
-                      min: 0,
-                      max: 120,
-                      initValue: 20,
-                    ),
-                    AgeAndWeight(
-                      title: "  Weight  (Kg)  ",
-                      onChange: (weightVal) {
-                        _weight = weightVal;
-                      },
-                      min: 0,
-                      max: 300,
-                      initValue: 50,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  child: SwipeableButtonView(
-                      isFinished: _isFinished,
-                      onFinish: () async {
-                        await Navigator.push(
-                            context,
-                            PageTransition(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Gender(
+                        onChanged: (genderVal) {
+                          _gender = genderVal;
+                        },
+                      ),
+                      Height(
+                        onChanged: (heightVal) {
+                          _height = heightVal;
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AgeAndWeight(
+                            title: "Age",
+                            onChange: (ageVal) {
+                              _age = ageVal;
+                            },
+                            min: 0,
+                            max: 120,
+                            initValue: 20,
+                          ),
+                          AgeAndWeight(
+                            title: "  Weight  (Kg)  ",
+                            onChange: (weightVal) {
+                              _weight = weightVal;
+                            },
+                            min: 0,
+                            max: 300,
+                            initValue: 50,
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 40),
+                        child: SwipeableButtonView(
+                          isFinished: _isFinished,
+                          onFinish: () async {
+                            await Navigator.push(
+                              context,
+                              PageTransition(
                                 child:
                                     ResultPage(bmiScore: _bmiScore, age: _age),
-                                type: PageTransitionType.fade));
-                        setState(() {
-                          _isFinished = false;
-                        });
-                      },
-                      onWaitingProcess: () {
-                        calculateBmi();
-                        Future.delayed(Duration(seconds: 1), () {
-                          setState(() {
-                            _isFinished = true;
-                          });
-                        });
-                      },
-                      activeColor: Colors.blue,
-                      buttonWidget: Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        color: Colors.black,
+                                type: PageTransitionType.fade,
+                              ),
+                            );
+                            setState(() {
+                              _isFinished = false;
+                            });
+                          },
+                          onWaitingProcess: () {
+                            calculateBmi();
+                            Future.delayed(Duration(seconds: 1), () {
+                              setState(() {
+                                _isFinished = true;
+                              });
+                            });
+                          },
+                          activeColor: Colors.blue,
+                          buttonWidget: const Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.black,
+                          ),
+                          buttonText: "CALCULATE",
+                        ),
                       ),
-                      buttonText: "CALCULATE"),
-                )
-              ],
-            ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.facebookF,
+                                color: Colors.blue),
+                            onPressed: () => _launchURL(
+                                'https://www.facebook.com/profile.php?id=100008335519291'), // Replace with your Facebook profile URL
+                          ),
+                          const SizedBox(width: 20), // Space between the icons
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.github,
+                                color: Colors.black),
+                            onPressed: () => _launchURL(
+                                'https://github.com/peterelia22'), // Replace with your GitHub profile URL
+                          ),
+                          const SizedBox(width: 20), // Space between the icons
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.linkedinIn,
+                                color: Colors.blue),
+                            onPressed: () => _launchURL(
+                                'https://www.linkedin.com/in/peter-elia-648485240/'), // Replace with your LinkedIn profile URL
+                          ),
+
+                        ]
+                      ),SizedBox(
+                        height: 30,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -119,5 +162,14 @@ class _HomePageState extends State<HomePage> {
 
   void calculateBmi() {
     _bmiScore = _weight / pow(_height / 100, 2);
+  }
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      print('Could not launch $url');
+    }
   }
 }
--- lib/views/result_page.dart
+++ lib/views/result_page.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_share/flutter_share.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pretty_gauge/pretty_gauge.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResultPage extends StatelessWidget {
   final double bmiScore;
   final int age;
-  String? bmiStatus;
-  String? bmiInterpretation;
-  Color? bmiStatusColor;
+  final String bmiStatus;
+  final String bmiInterpretation;
+  final Color bmiStatusColor;
 
-  ResultPage({super.key, required this.bmiScore, required this.age});
+  ResultPage({
+    super.key,
+    required this.bmiScore,
+    required this.age,
+  })  : bmiStatus = _calculateBmiStatus(bmiScore),
+        bmiInterpretation = _calculateBmiInterpretation(bmiScore),
+        bmiStatusColor = _calculateBmiStatusColor(bmiScore);
 
   @override
   Widget build(BuildContext context) {
-    setBmiInterpretation();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -27,101 +33,138 @@ class ResultPage extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(12),
-        child: Card(
-          elevation: 12,
-          shape: RoundedRectangleBorder(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Your Score",
-                style: TextStyle(color: Colors.blue, fontSize: 30),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              PrettyGauge(
-                gaugeSize: 300,
-                minValue: 0,
-                maxValue: 40,
-                segments: [
-                  GaugeSegment('Under Weight', 18.5, Colors.red),
-                  GaugeSegment('Normal', 6.4, Colors.green),
-                  GaugeSegment('Over Weight', 5, Colors.orange),
-                  GaugeSegment('Obese', 10.1, Colors.pink)
-                ],
-                valueWidget: Text(
-                  bmiScore.toStringAsFixed(1),
-                  style: TextStyle(fontSize: 50),
-                ),
-                currentValue: bmiScore.toDouble(),
-                needleColor: Colors.blue,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                bmiStatus!,
-                style: TextStyle(fontSize: 20, color: bmiStatusColor!),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                bmiInterpretation!,
-                style: TextStyle(fontSize: 15),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              elevation: 12,
+              shape: RoundedRectangleBorder(),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("Re-Calculate"),
+                  Text(
+                    "Your Score",
+                    style: TextStyle(color: Colors.blue, fontSize: 30),
                   ),
-                  SizedBox(
-                    width: 10,
+                  SizedBox(height: 10),
+                  PrettyGauge(
+                    gaugeSize: 300,
+                    minValue: 0,
+                    maxValue: 40,
+                    segments: [
+                      GaugeSegment('Under Weight', 18.5, Colors.red),
+                      GaugeSegment('Normal', 6.4, Colors.green),
+                      GaugeSegment('Over Weight', 5, Colors.orange),
+                      GaugeSegment('Obese', 10.1, Colors.pink),
+                    ],
+                    valueWidget: Text(
+                      bmiScore.toStringAsFixed(1),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                    currentValue: bmiScore.toDouble(),
+                    needleColor: Colors.blue,
                   ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await FlutterShare.share(
-                        title: 'BMI Score',
-                        text:
-                            "Your BMI Score is ${bmiScore.toStringAsFixed(1)} at Age $age. Status: $bmiStatus. Interpretation: $bmiInterpretation",
-                      );
-                    },
-                    child: Text("Share"),
+                  SizedBox(height: 10),
+                  Text(
+                    bmiStatus,
+                    style: TextStyle(fontSize: 20, color: bmiStatusColor),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    bmiInterpretation,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("Re-Calculate"),
+                      ),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await FlutterShare.share(
+                            title: 'BMI Score',
+                            text:
+                                "Your BMI Score is ${bmiScore.toStringAsFixed(1)} at Age $age. Status: $bmiStatus. Interpretation: $bmiInterpretation",
+                          );
+                        },
+                        child: Text("Share"),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.facebook, color: Colors.blue),
+                  onPressed: () => _launchURL(
+                      'https://www.facebook.com/profile.php?id=100008335519291'),
+                ),
+                SizedBox(width: 20),
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.github),
+                  onPressed: () => _launchURL(
+                      'https://www.linkedin.com/in/peter-elia-648485240/'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 
-  void setBmiInterpretation() {
+  static String _calculateBmiStatus(double bmiScore) {
     if (bmiScore > 30) {
-      bmiStatus = "Obese";
-      bmiInterpretation = "Please Work To Reduce Obesity";
-      bmiStatusColor = Colors.pink;
+      return "Obese";
     } else if (bmiScore >= 25) {
-      bmiStatus = "Over Weight";
-      bmiInterpretation = "Do Regular Exercise And Reduce The Weight";
-      bmiStatusColor = Colors.orange;
+      return "Over Weight";
     } else if (bmiScore >= 18.5) {
-      bmiStatus = "Normal";
-      bmiInterpretation = "Enjoy, You Are Fit";
-      bmiStatusColor = Colors.green;
-    } else if (bmiScore < 18.5) {
-      bmiStatus = "Under Weight";
-      bmiInterpretation = "Try To Increase The Weight";
-      bmiStatusColor = Colors.red;
+      return "Normal";
+    } else {
+      return "Under Weight";
+    }
+  }
+
+  static String _calculateBmiInterpretation(double bmiScore) {
+    if (bmiScore > 30) {
+      return "Please Work To Reduce Obesity";
+    } else if (bmiScore >= 25) {
+      return "Do Regular Exercise And Reduce The Weight";
+    } else if (bmiScore >= 18.5) {
+      return "Enjoy, You Are Fit";
+    } else {
+      return "Try To Increase The Weight";
+    }
+  }
+
+  static Color _calculateBmiStatusColor(double bmiScore) {
+    if (bmiScore > 30) {
+      return Colors.pink;
+    } else if (bmiScore >= 25) {
+      return Colors.orange;
+    } else if (bmiScore >= 18.5) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      print('Could not launch $url');
     }
   }
 }
